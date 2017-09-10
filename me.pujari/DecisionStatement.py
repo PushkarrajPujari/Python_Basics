2f5a164314480a309695d0e15b880d7834dbf65d
PushkarrajPujari <pushkarraj@pujari.me>
2017-09-10 15:33:26 +0530

    Stage 4 - simulating switch using dict

--- me.pujari/DecisionStatement.py
+++ me.pujari/DecisionStatement.py
@@ -1,18 +1,23 @@
-# if ... elif ... else .. example
-marks = 50
-if marks < 0 or marks > 100:
-    print("Invalid marks , Please enter valid marks")
-elif 0 <= marks < 35:
-    print("Sorry , You Have failed")
-elif 35 <= marks < 50:
-    print("You have cleared the exam with Passed Class")
-elif 50 <= marks < 60:
-    print("You have cleared the exam with Second Class")
-elif 60 <= marks < 75:
-    print("You have cleared the exam with First Class")
-elif 75 <= marks < 90:
-    print("You have cleared the exam with distinction")
-elif 90 <= marks <= 100:
-    print("You have cleared the exam with merit")
-else:
-    print("Something wrong ....")
+# Simulating Switch statement in python using dict
+
+def welcome():
+    print("Hello , nice to meet you whats up")
+
+
+def goodbye():
+    print("Ok goodbye... , stay in touch good night")
+
+
+def unknown():
+    print("Sorry I didn't get you can you repeat")
+
+
+switch = {
+    "hi": welcome,
+    "bye": goodbye,
+    "": unknown
+}
+
+message = input("Enter your message")
+
+switch[message]()
