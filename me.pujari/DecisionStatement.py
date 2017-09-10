77608ff585aeae04b3dfe561e1f4c2fbc1b26c7b
PushkarrajPujari <pushkarraj@pujari.me>
2017-09-10 15:14:34 +0530

    Stage 3.1 - Bugs fixed checkout this commit instead of stage 3 Simple if... elif.. else.. using marks example

--- me.pujari/DecisionStatement.py
+++ me.pujari/DecisionStatement.py
@@ -1,16 +1,18 @@
-# Demonstrating if elsif with exam marks example 
-marks = 58
+# if ... elif ... else .. example
+marks = 50
 if marks < 0 or marks > 100:
     print("Invalid marks , Please enter valid marks")
-elif 0 < marks < 35:
+elif 0 <= marks < 35:
     print("Sorry , You Have failed")
-elif 35 < marks < 50:
+elif 35 <= marks < 50:
     print("You have cleared the exam with Passed Class")
-elif 50 < marks < 60:
+elif 50 <= marks < 60:
     print("You have cleared the exam with Second Class")
-elif 60 < marks < 75:
+elif 60 <= marks < 75:
     print("You have cleared the exam with First Class")
-elif 75 < marks < 90:
+elif 75 <= marks < 90:
     print("You have cleared the exam with distinction")
-else:
+elif 90 <= marks <= 100:
     print("You have cleared the exam with merit")
+else:
+    print("Something wrong ....")
